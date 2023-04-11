8b017e13a1d1a92f7a1fece330cdf9a206408427
MLJut <ml.jut@student.avans.nl>
2023-04-11 11:10:19 +0200

    implement bubblesort

---
+++ Swen2Sort/Swen2Sort/inc/bubblesort.h
@@ -0,0 +1,2 @@
+#pragma once
+void bubbleSort(int* arr, int size);
---
+++ Swen2Sort/Swen2Sort/src/bubblesort.cpp
@@ -0,0 +1,13 @@
+#include "../inc/bubblesort.h"
+
+void bubbleSort(int* arr, int size) {
+	for (int m = size - 1; m > 0; m--) { //loop through the entire array backwards
+		for (int n = 0; n < m; n++) { //for every step through array, loop through array and move number towards back if it's larger than the next value
+			if (arr[n] > arr[n + 1]) {
+				int temp = arr[n];
+				arr[n] = arr[n + 1];
+				arr[n + 1] = temp;
+			}
+		}
+	}
+}
